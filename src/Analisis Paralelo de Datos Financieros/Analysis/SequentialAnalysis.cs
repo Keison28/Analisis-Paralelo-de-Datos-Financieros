c5ab68752f57cdc4d54f6f1958ef6c843736c8a8
RyanOrtiz0246 <ryan142310@gmail.com>
2025-12-12 18:07:29 -0400

    Revert "UpdateTheSystem Co-Authored-By<RyanOrtizcampos>"
    This reverts commit 30c64fa9a4e203987fa24736cabc42b994a4b92c.

--- src/Analisis Paralelo de Datos Financieros/Analysis/SequentialAnalysis.cs
+++ src/Analisis Paralelo de Datos Financieros/Analysis/SequentialAnalysis.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 namespace Analisis_Paralelo_de_Datos_Financieros.Analysis
 {
-<<<<<<< Updated upstream
     internal class SequentialAnalysis
     {
         public class AnalysisResult
@@ -69,39 +68,6 @@ namespace Analisis_Paralelo_de_Datos_Financieros.Analysis
                 AverageReturn = avgReturn
             };
 
-=======
-    public class SequentialAnalysis
-    {
-        public ResultadoAnalisis Analizar(List<double> data)
-        {
-            double promedio = Statistics.Mean(data);
-            double volatilidad = Statistics.Volatility(data);
-            double max = Statistics.Max(data);
-            double min = Statistics.Min(data);
-            double retornoProm = Statistics.AverageReturn(data);
-            double varianza = Statistics.Variance(data);
-            double desviacionEstandar = Statistics.StdDev(data);
-
-            return new ResultadoAnalisis
-            {
-                Promedio = promedio,
-                Volatilidad = volatilidad,
-                Maximo = max,
-                Minimo = min,
-                RetornoPromedio = retornoProm,
-                DesviacionEstandar = desviacionEstandar
-            };
-        }
-
-        public class ResultadoAnalisis
-        {
-            public double Promedio { get; set; }
-            public double Volatilidad { get; set; }
-            public double Maximo { get; set; }
-            public double Minimo { get; set; }
-            public double RetornoPromedio { get; set; }
-            public double DesviacionEstandar { get; set; }
->>>>>>> Stashed changes
         }
     }
 }
